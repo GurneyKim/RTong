49567e2047c38d1370dfae405a932126c66edf6e
hellogurney <hellogurney@gmail.com>
2018-05-12 22:12:30 +0900

    .
    .

---
+++ Part1/4회/분석 결과 정리와 공유.R
@@ -0,0 +1,12 @@
+#### 분석 결과 정리와 공유 ####
+# 따라하며 배우는 데이터 과학 12장 PPT: https://goo.gl/mzAGqT
+
+#### Markdown (마크다운) ####
+# 마크다운이란 ? : https://goo.gl/fD10fA
+# 마크다운 문법 : https://goo.gl/hz7Dt1
+# 마크다운 연습 : http://markdownlivepreview.com/
+
+#### R Markdown ####
+# cheat sheet : https://goo.gl/uUHJq6
+# ex : https://rpubs.com/jmhome/datatype_analysis
+# RPub : https://goo.gl/6oQRJa
